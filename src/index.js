e1050a1d3fd76ac21af2cc6ef8b0da3b820cc65a
Denilson Nascimento <denilsonn7187@gmail.com>
2024-08-07 21:46:22 -0300

    Comentando e inserindo metodo Post para cadastrar uma nova atividade.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-//Mock
-
+//Mock de Dados
 const tarefas = [
     {id: 1, titulo: "Estudar", descricao: "Estudar bancos de dados relacionais", status: "pendente"},
     {id: 2, titulo: "Cozinhar", descricao: "Fazer Marmita do dia", status: "pendente"},
@@ -15,8 +15,15 @@ app.get('/', (req,res)=> {
     res.send('Hello World');
 });
 
+//Lista todas as atividades
 app.get('/tasks', (req,res)=> {
-    res.send(tarefas);
+    res.status(200).send(tarefas);
+});
+
+//Insere uma nova atividade na lista de atividades, através do corpo da requisição.
+app.post('/tasks', (req,res)=> {
+    tarefas.push(req.body)
+    res.status(200).send('Atividade criada');
 });
 
 export default app;
