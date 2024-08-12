59e90d54adf3189e64841c9b8999e0c872ed0258
Denilson Nascimento <denilsonn7187@gmail.com>
2024-08-12 19:36:02 -0300

    Inserindo variaveis de ambiente com dotenv, para nao deixar dados sensiveis expostos e fazendo conexao com baco de dados

---
+++ infra/conexao.js
@@ -0,0 +1,15 @@
+import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const conexao = mysql2.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PAS,
+    database: process.env.DB_NAME
+});
+
+
+export default conexao;
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import app from "./src/index.js";
+import conexao from "./infra/conexao.js";
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server rodando na porta ${PORT}`);
-});
+//fazer a conexao no data base
+conexao.connect((erro) => {
+    if(erro){
+        console.log(erro);
+    } else {
+        console.log('Conexao realizada com sucesso.')
+        //Nesse caso o server só vai subir se nos conectarmos corretamente no banco de dados.
+        app.listen(PORT, () => {
+            console.log(`Server rodando na porta ${PORT}`);
+        });
+    }
+})
+
