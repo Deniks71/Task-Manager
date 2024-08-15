82a5efdaab6b35e4b70ec4ab56b2024c4852d913
Denilson Nascimento <denilsonn7187@gmail.com>
2024-08-15 20:03:35 -0300

    mudando posicionamento de alguns elementos e nome de uma pasta

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import express from 'express';
-import conexao from '../infra/conexao.js';
+import conexao from '../database/conexao.js';
 
 const app = express();
 app.use(express.json());
 
 //Cria uma nova Task
 app.post('/tasks', (req,res)=> {
-    //tarefas.push(req.body)
-    //res.status(200).send('Atividade criada');
     const task = req.body;
     const sql = 'INSERT INTO bdtaskmanager.tasks SET ?;'
     conexao.query(sql, task, (erro, resultado)=> {
         if(erro) {
             console.log(erro);
-            res.status(400).json({ 'erro': erro })
+            res.status(404).json({ 'erro': erro })
         } else {
             res.status(201).json(resultado);
         }
@@ -51,12 +49,6 @@ app.get('/tasks/:id', (req,res)=>{
 
 //Atualiza Task
 app.put('/tasks/:id', (req,res)=> {
-    // let index = buscaIndexDaTask(req.params.id);
-    // tarefas[index].titulo = req.body.titulo;
-    // tarefas[index].descricao = req.body.descricao;
-    // tarefas[index].status = req.body.status;
-    // res.json(tarefas)
-    //UPDATE `bdtaskmanager`.`tasks` SET `titulo` = 'Estudar' WHERE (`id` = '3');
     const id = req.params.id;
     const task = req.body;
     const sql = 'UPDATE bdtaskmanager.tasks SET ? WHERE id=?;'
@@ -89,24 +81,5 @@ app.delete('/tasks/:id', (req,res)=> {
         }
     });
 })
-
-
-//Função para achar um ID igual(OBS, pode ser usada a Funçao pre pronta do javascript "filter")
-function buscaTaskPorId (id){
-    for(let i = 0; i < tarefas.length; i++) {
-        if(tarefas[i].id == id){
-            return tarefas[i]
-        }
-    }
-}
-
-//Função para achar o index para saber a opcao da atividade de um ID especifico.
-function buscaIndexDaTask(id){
-    for(let i = 0; i < tarefas.length; i++){
-        if(tarefas[i].id == id){
-            return i;
-        }
-    }
-}
 
 export default app;
