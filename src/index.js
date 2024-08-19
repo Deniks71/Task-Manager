1dd0ae1c6ba808fd460dfda17b623ab469412f91
Denilson Nascimento <denilsonn7187@gmail.com>
2024-08-19 17:45:36 -0300

    Refatorando o codigo, realizando crud com metodos HTTP no Database utilizando codigo assincrono

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ const app = express();
 app.use(express.json());
 
 //Cria uma nova Task
-app.post('/tasks', (req,res)=> {
+app.post('/tasks', async (req,res)=> {
     const task = req.body;
     const sql = 'INSERT INTO bdtaskmanager.tasks SET ?;'
-    conexaoPromise.query(sql, task, (erro, resultado)=> {
-        if(erro) {
-            console.log(erro);
-            res.status(404).json({ 'erro': erro })
-        } else {
-            res.status(201).json(resultado);
-        }
-    });
+    try {
+        const [resultado] = await conexaoPromise.query(sql,task);
+        res.status(201).json(resultado);
+    } catch(erro) {
+        console.log(erro);
+        res.status(404).json({ 'erro': erro })
+    }
 });
 
 //Lista todas as tasks
@@ -30,54 +29,45 @@ app.get('/tasks', async (req,res)=> {
 });
 
 //Lista uma tarefa especifica
-app.get('/tasks/:id', (req,res)=>{
-    //res.json(buscaTaskPorId(req.params.id));
+app.get('/tasks/:id', async (req,res)=>{
     const id = req.params.id
     const sql = 'SELECT * FROM bdtaskmanager.tasks WHERE id=?;'
-    conexaoPromise.query(sql, id, (erro, resultado)=> {
-        if(erro) {
-            console.log(erro);
-            res.status(404).json({ 'erro': erro })
-        } else {
-            //A resposta do database é um array, por isso que utilizei o indice 0
-            res.json(resultado[0]);
-        }
-    });
+    try {
+        const [resultado] = await conexaoPromise.query(sql,id);
+        res.status(200).json(resultado[0]);
+    } catch(erro) {
+        res.status(404).json({ 'erro': erro })
+    }
+
 });
 
 //Atualiza Task
-app.put('/tasks/:id', (req,res)=> {
+app.put('/tasks/:id', async (req,res)=> {
     const id = req.params.id;
     const task = req.body;
     const sql = 'UPDATE bdtaskmanager.tasks SET ? WHERE id=?;'
-    conexaoPromise.query(sql, [task,id], (erro, resultado)=> {
-        if(erro) {
-            console.log(erro);
-            res.status(404).json({ 'erro': erro })
-        } else {
-            //A resposta do database é um array, por isso que utilizei o indice 0
-            res.status(200).json(resultado);
-        }
-    });
+    try{
+        const [resultado] = await conexaoPromise.query(sql,[task,id]);
+        res.status(200).json(resultado);
+    } catch(erro) {
+        console.log(erro);
+        res.status(404).json({ 'erro': erro });
+    };
+
 })
 
 //deleta task
-app.delete('/tasks/:id', (req,res)=> {
-    // let index = buscaIndexDaTask(req.params.id);
-    // tarefas.splice(index,1)
-    // res.send(`Atividade com ID ${req.params.id} Exluida com sucesso`);
-    // DELETE FROM `bdtaskmanager`.`tasks` WHERE (`id` = '4');
+app.delete('/tasks/:id', async (req,res)=> {
     const id = req.params.id
     const sql = 'DELETE FROM bdtaskmanager.tasks WHERE id=?;'
-    conexaoPromise.query(sql, id, (erro, resultado)=> {
-        if(erro) {
-            console.log(erro);
-            res.status(404).json({ 'erro': erro })
-        } else {
-            //A resposta do database é um array, por isso que utilizei o indice 0
-            res.status(200).json(resultado);
-        }
-    });
+    try {
+        const [resultado] = await conexaoPromise.query(sql,id);
+        res.status(200).json(resultado);
+    } catch(erro) {
+        console.log(erro);
+        res.status(404).json({ 'erro': erro })
+    }
+   
 })
 
 export default app;
